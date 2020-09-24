c035788431628b0e3f31973eb8f92992fd6359eb
Akanksha Saran <akankshasaran@gmail.com>
2020-09-24 10:38:14 -0500

    more args

--- atari/final_evaluations.py
+++ atari/final_evaluations.py
@@ -11,20 +11,24 @@ if __name__ == '__main__':
 
     parser.add_argument("--all_games", action="store_true", default=False)
     parser.add_argument("--dir", type=str, default='')
+    parser.add_argument("--ckpt", type=int, default=43000)
+    parser.add_argument("--weight", type=float, default=0.001)
+    parser.add_argument("--seed", type=int, default=0)
 
     args = parser.parse_args()
 
     if args.all_games:
         env_names = ['alien','asterix','bank_heist','berzerk','breakout','centipede','demon_attack','enduro','freeway','frostbite','hero','montezuma_revenge','mspacman','name_this_game','phoenix','riverraid','road_runner','seaquest','space_invaders','venture']
     else:
-        env_names = ['asterix','breakout','centipede','phoenix','seaquest','mspacman']
+        env_names = ['asterix','breakout','centipede','phoenix','mspacman','seaquest']
 
     avgs, stds = [], []
 
     files = os.listdir(args.dir)
     for env in env_names:
         empty = False
-        result_file = os.path.join(args.dir,env+'_bc_results.txt')
+        result_file = os.path.join(args.dir,env+'ppo_models_KL_'+env+'_KL_'+\
+            str(args.weight)+'_seed'+str(args.seed)+'_checkpoints_'+str(args.ckpt)+'_evaluation.txt')
         if not os.path.isfile(result_file):
             print('File does not exist: ', result_file)
             empty = True
